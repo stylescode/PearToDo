7f83ba78ac9b0a55bb3436dc535ee66c0afbd8b1
Styles <stylesk11@gmail.com>
2024-09-17 01:41:59 -0700

    use express json + cors

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 const routes = require('./server/routes');
+
+app.use(cors());
+app.use(express.json());
 
 // Serve static files from the client app
 app.use(express.static(path.join(__dirname, './client/dist')));
