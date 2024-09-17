3ba2e5858616b62adcd5be0781fef813ea760b56
Styles <stylesk11@gmail.com>
2024-09-16 19:51:37 -0700

    route todo requests

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const helloRoutes = require('./hello');
+const todoRoutes = require('./todos');
 
 router.use('/hello', helloRoutes);
+router.use('/todos', todoRoutes);
 
 module.exports = router;
